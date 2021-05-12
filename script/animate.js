c85d51fefc85bb8694c1022d831312c5ee2731ce
AzizBooker <bookeraziz@gmail.com>
2021-05-12 09:30:12 -0400

    update animation

--- script/animate.js
+++ script/animate.js
@@ -10,12 +10,13 @@ timeline.from('#logo',{
     duration: 1.5,
     opacity: 0,
     y: -50,
-}).from('.title-contanier-1-text',{
+}).from('.title-contanier-1-text ',{
     opacity:0,
     y:30
 }).from('.floatTL',{
     duration:1.2,
-    opacity:0
+    opacity:0,
+    scale:1.1,
 }).from(".title-contanier-2",{
     opacity:0
 })
