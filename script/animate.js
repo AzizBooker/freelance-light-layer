289bbe1e8a74eedb967974722ae27f198d5bd495
AzizBooker <bookeraziz@gmail.com>
2021-05-12 08:40:28 -0400

    first commit

---
+++ script/animate.js
@@ -0,0 +1,21 @@
+console.log('Hello')
+
+gsap.defaults({
+  duration: 1,
+});
+
+const timeline=gsap.timeline({delay: 1})
+
+timeline.from('#logo',{
+    duration: 1.5,
+    opacity: 0,
+    y: -50,
+}).from('.title-contanier-1-text',{
+    opacity:0,
+    y:30
+}).from('.floatTL',{
+    duration:1.2,
+    opacity:0
+}).from(".title-contanier-2",{
+    opacity:0
+})
